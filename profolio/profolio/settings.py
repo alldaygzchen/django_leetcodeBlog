7a0c24a9878d9fd60f36465c121552c7864840bb
alldaygzchen <gonzalochen66@gmail.com>
2023-01-20 12:32:35 +0800

    move files

--- profolio/profolio/settings.py
+++ profolio/profolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=dz0$k4bn&^#5@f))3y#w3_u&r+o-qwpsyyd%zsle)!j)7tt#t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_PRODUCTION",True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
@@ -117,6 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+
+STATIC_ROOT = BASE_DIR /"staticfiles"
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
